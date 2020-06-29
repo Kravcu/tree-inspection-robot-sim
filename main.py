d4bd76216204bae22f5031da42af85536d195323
Tomasz Krawczyk <kravcu2@gmail.com>
2020-06-29 20:54:26 +0200

    Added some shit.
    Signed-off-by: Tomasz Krawczyk <kravcu2@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import copy
 import random
 from enum import Enum
 from typing import List
@@ -40,23 +39,35 @@ class Position:
         self.y = y
 
     def move_left(self):
-        self.x += -3
-        self.x = -self.x
+        self.x += -1
 
     def move_right(self):
-        self.x += 3
+        self.x += 1
 
     def move_up(self):
-        self.y += 3
+        self.y += 1
 
     def move_down(self):
-        self.y += -3
+        self.y += -1
 
     def get_x(self):
         return self.x
 
     def get_y(self):
         return self.y
+
+    def reposition(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_pos(self):
+        return self.x, self.y
+
+    def __cmp__(self, other):
+        if self.x == other.x and self.y == other.y:
+            return True
+        else:
+            return False
 
 
 class Robot:
@@ -80,7 +91,7 @@ class Robot:
         else:
             self.position.move_right()
         self.check_position()
-        self.pos_history.append(copy.copy(self.position))
+        self.pos_history.append(self.position.get_pos())
 
     def walk(self, direction):
         if direction == "left":
@@ -91,8 +102,8 @@ class Robot:
             self.position.move_up()
         elif direction == "down":
             self.position.move_down()
-        self.check_position()
-        self.pos_history.append(copy.copy(self.position))
+        # self.check_position()
+        self.pos_history.append(self.position.get_pos())
 
     def check_position(self):
         if self.position.x < 0:
@@ -112,21 +123,81 @@ class Robot:
 
 
 class Simulation:
-    def __init__(self, tree_map=0):
+    def __init__(self, tree_map: np.ndarray = 0):
         self.img = plt.imread('forest.png')
         self.tree_map = tree_map
+
         img_x, img_y = len(self.img[0]), len(self.img)
+        self.visited = [[0 for x in range(img_x)] for y in range(img_y)]
         plt.imshow(self.img, origin={0, 0})
 
-        self.workers: List[Robot] = [Robot(np.random.rand(img_x, img_y), self.tree_map, 600, 430)]
+        self.workers: List[Robot] = [Robot(np.random.rand(img_x, img_y), self.tree_map, 430, 430),
+                                     Robot(np.random.rand(img_x, img_y), self.tree_map, 800, 430)]
         for i in range(10000):
-            self.workers[0].random_walk()
-        plt.scatter([elem.get_x() for elem in self.workers[0].pos_history],
-                    [elem.get_y() for elem in self.workers[0].pos_history], s=5)
+            workers_to_remove = []
+            for index, worker in enumerate(self.workers):
+                possible_moves = []
+                x = worker.position.get_x()
+                y = worker.position.get_y()
+                if random.randint(0, 100) < 2:
+                    print(f"Worker {index} został zniszczony przez studia na AGH.")
+                    decision = input("would you like to [remove] or [reposition]? ")
+                    decision = input(f"Would you like to [remove] or [reposition]? ")
+                    if decision == "remove":
+                        workers_to_remove.append(index)
+                    elif decision == "reposition":
+                        x = int(input("Please put new X pos "))
+                        y = int(input("Please put new Y pos "))
+                        self.workers[index].position.reposition(x, y)
+                else:
+                    try:
+                        if (self.visited[x + 1][y] == 0 and x + 1 <= img_x) or (x + 1, y) in worker.pos_history:
+                            possible_moves.append('right')
+                    except IndexError:
+                        pass
+                    try:
+                        if (self.visited[x - 1][y] == 0 and x - 1 >= 0) or (x - 1, y) in worker.pos_history:
+                            possible_moves.append('left')
+                    except IndexError:
+                        pass
+                    try:
+                        if (self.visited[x][y + 1] == 0 and y + 1 <= img_y) or (x, y + 1) in worker.pos_history:
+                            possible_moves.append('up')
+                    except IndexError:
+                        pass
+                    try:
+                        if (self.visited[x][y - 1] == 0 and y - 1 >= 0) or (x, y - 1) in worker.pos_history:
+                            possible_moves.append('down')
+                    except IndexError:
+                        pass
+
+                    if not possible_moves:
+                        print(f"Worker {index} has run out of moves.")
+                        decision = input(f"Would you like to [remove] or [reposition]? ")
+                        if decision == "remove":
+                            workers_to_remove.append(index)
+                        elif decision == "reposition":
+                            x = int(input("Please put new X pos "))
+                            y = int(input("Please put new Y pos "))
+                            self.workers[index].position.reposition(x, y)
+                    else:
+                        choice = random.choice(possible_moves)
+                        self.workers[index].walk(choice)
+                    self.visited[worker.position.get_x()][worker.position.get_y()] = 1
+                    if self.tree_map[worker.position.get_x()][worker.position.get_y()] == 1:
+                        print("znalazlem se drzewo. zajebiscie")
+                        plt.scatter(worker.position.get_x(), worker.position.get_y(), s=10)
+                for elem in workers_to_remove:
+                    self.workers.remove(elem)
+
+        # TODO add dynamic plotting of workers position
+        plt.scatter(worker.position.get_x(), worker.position.get_y(), s=5)
         plt.title('Tree inspection robot simulation')
         plt.xlabel('x')
         plt.ylabel('y')
         plt.show()
 
 
-sim = Simulation()
+img = plt.imread('forest.png')
+img_x, img_y = len(img[0]), len(img)
+sim = Simulation(tree_map=np.random.rand(img_x, img_y))
